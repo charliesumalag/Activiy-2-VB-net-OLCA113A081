c2e0914a5eb0a57236ed0ed90c8e19824eacb431
charliesumalag <121892017+charliesumalag@users.noreply.github.com>
2023-10-02 15:06:54 +0800

    Add files via upload
    first commit

---
+++ Activity - 2/Activity - 2/Module1.vb
@@ -0,0 +1,20 @@
+﻿Module Module1
+
+    Sub Main()
+        'declaration start
+        Dim fullName = "Charlie P. Sumalag"
+        Dim course = "Bachelor of Science in Information Technology"
+        Dim section = "OLCA113A081"
+        Dim homeAddress = "Block 4 lot 16 Felicidad 4 Burgos, Rodriguez, Rizal"
+        'declaration end
+
+        'output start
+        Console.WriteLine(fullName)
+        Console.WriteLine(course)
+        Console.WriteLine(section)
+        Console.WriteLine(homeAddress)
+        Console.ReadLine() 'pause purpose for output display
+        'output end
+    End Sub
+
+End Module
